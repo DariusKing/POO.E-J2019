620f78dc9e9287b4774ca059fa4f582040d0df1f
Juan Hernandez <Jarceluz@gmail.com>
2019-02-15 10:59:53 -0800

    Practica sobre encapsulamiento

---
+++ Encapsulamiento/Clase1-POO/FiguraGeometrica.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Clase1_POO
+{
+    public class FiguraGeometrica
+    {
+        private int id;
+        private string nombre;
+        private double lado1;
+        private double lado2;
+        private string descripcion;
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        public double Lado1
+        {
+            get { return lado1; }
+            set { lado1 = value; }
+        }
+        public double Lado2
+        {
+            get { return lado2; }
+            set { lado2 = value; }
+        }
+        public string Descripcion
+        {
+            get { return descripcion; }
+            set { descripcion = value; }
+        }
+        public void CrearFiguraGeometrica(FiguraGeometrica nuevaFigura)
+        {
+
+        }
+
+        public void CalcularArea(FiguraGeometrica figura)
+        {
+
+        }
+        public void CalcularPerimetro(FiguraGeometrica figura)
+        {
+
+        }
+
+    }
+}
---
+++ Encapsulamiento/Clase1-POO/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Clase1_POO
+{
+    class Program
+    {
+        //static int valor = 67;
+        //public static void CalcularMetros()
+        //{
+
+        //}
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+            FiguraGeometrica Cuadrado = new FiguraGeometrica();
+            Cuadrado.Id = 1;
+            Console.WriteLine(Cuadrado.Id);
+            Console.ReadKey();
+            FiguraGeometrica Rectangulo = new FiguraGeometrica();
+
+        }
+    }
+}
